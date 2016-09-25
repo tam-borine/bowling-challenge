ddeb7806368a38458e4d1c2239b040d11205744e
Tam <tamborine@Tams-Air.home>
2016-09-25 18:17:27 +0100

    still extracting animation into classes

--- Asteroid.js
+++ Asteroid.js
@@ -14,3 +14,33 @@ var Asteroid = function(x, y, radius, mass, friction) {
 
     this.player = false;
 };
+
+var buildAsteroids = function() {
+
+      // Set up player asteroid
+      var pX = playerOriginalX;
+      var pY = playerOriginalY;
+      var pRadius = 15;
+      var mass = 10;
+      var friction = 0.97;
+      player = new Asteroid(pX, pY, pRadius, mass, friction);
+      player.player = true;
+      asteroids.push(player);
+
+      // Set up other asteroids
+      //var outerRing = 8; // Asteroids around outer ring
+      //var ringCount = 3; // Number of rings
+      var nPinRows = 4;
+      var distancePins = 40;
+      for (var pinRow = 0; pinRow < nPinRows; ++pinRow) {
+        for (var pinCol = 0; pinCol <= pinRow; ++pinCol) {
+          y = platformY - pinRow*Math.sqrt(3/4)*distancePins;
+          x = platformX + (-pinRow/2 + pinCol)*distancePins;
+          var radius = 10;
+          var mass = 5;
+          var friction = 0.95;
+          asteroids.push(new Asteroid(x, y, radius, mass, friction));
+        }
+      }
+
+}
--- animation.js
+++ animation.js
@@ -41,11 +41,20 @@ function resetPlayer() {
     player.vY = 0;
 };
 
+var midGame = function() {
+
+  uiReset.click(function(e) {
+      e.preventDefault();
+      uiComplete.hide();
+      startGame();
+  });
+}
 // Reset and start the game
 function startGame() {
     // Reset game stats
     uiRoll.html("0");
     uiFrameStats.show();
+    midGame();
 
     // Set up initial game settings
     roll = 0;
@@ -63,31 +72,7 @@ function startGame() {
     platformOuterRadius = 100;
     platformInnerRadius = 75;
 
-    // Set up player asteroid
-    var pX = playerOriginalX;
-    var pY = playerOriginalY;
-    var pRadius = 15;
-    var mass = 10;
-    var friction = 0.97;
-    player = new Asteroid(pX, pY, pRadius, mass, friction);
-    player.player = true;
-    asteroids.push(player);
-
-    // Set up other asteroids
-    //var outerRing = 8; // Asteroids around outer ring
-    //var ringCount = 3; // Number of rings
-    var nPinRows = 4;
-    var distancePins = 40;
-    for (var pinRow = 0; pinRow < nPinRows; ++pinRow) {
-      for (var pinCol = 0; pinCol <= pinRow; ++pinCol) {
-        y = platformY - pinRow*Math.sqrt(3/4)*distancePins;
-        x = platformX + (-pinRow/2 + pinCol)*distancePins;
-        var radius = 10;
-        var mass = 5;
-        var friction = 0.95;
-        asteroids.push(new Asteroid(x, y, radius, mass, friction));
-      }
-    }
+    buildAsteroids();
 
     uiPinsRemaining.html(asteroids.length-1); //remove player from Asteroid count
 
@@ -178,12 +163,14 @@ function startGame() {
 function init() {
     uiFrameStats.hide();
     uiComplete.hide();
+    uiIntro.show();
 
-    uiReset.click(function(e) {
-        e.preventDefault();
-        uiComplete.hide();
-        startGame();
-    });
+    uiPlay.click(function(e){
+      e.preventDefault();
+      uiComplete.hide();
+      startGame();
+    })
+
 };
 
 
